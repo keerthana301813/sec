fbb5c954a3cb2449522e52cb543fc6d23c6b0175
keerthana <keerthanagavvala2006@gmail.com>
2025-09-13 10:16:01 +0530

    jquery

---
+++ front end/new.js
@@ -0,0 +1,13 @@
+console.group("SRIT")
+let age=20;
+age=25;
+console.log(age);
+const givenname="srit";
+console.log(givenname)
+
+function generateWelcomeMessage(name){
+    return "Welcome,"+ name + "! Good morning";
+}
+
+let welcome_message = generateWelcomeMessage(givenname);
+console.log(welcome_message);
